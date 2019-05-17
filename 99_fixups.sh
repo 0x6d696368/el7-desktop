584820891ff0ff82cb9c63e14902481ed32827e7
0x6d696368 <>
2019-05-17 14:06:03 +0200

    Changes 99_fixups.sh

--- 99_fixups.sh
+++ 99_fixups.sh
@@ -4,9 +4,11 @@ sudo echo thx
 
 # N-mode on Intel Corporation Centrino Advanced-N 6205 is doggy; disable it
 if [[ $(lspci -n | grep -o "^.* 8086:0082 (rev [0-9]*)$") ]]; then
-echo "options iwlwifi 11n_disable=1" | sudo tee /etc/modprobe.d/iwlwifi.conf
-sudo modprobe -rfv iwlwifi
-sudo modprobe -v iwlwifi
+# FIXME: doesn't seem to help / doesn't seem to be the actual problem
+#echo "options iwlwifi 11n_disable=1" | sudo tee /etc/modprobe.d/iwlwifi.conf
+#sudo modprobe -rfv iwlwifi
+sudo modprobe -rfv iwldvm iwlwifi
+sudo modprobe -v iwlwifi iwldvm
 fi # if [[ $(lspci -n | grep -o "^.* 8086:0082 (rev [0-9]*)$") ]]; then
 
 
